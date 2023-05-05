1951a910f5b123ce3613b03497b2e53968c54425
XyanZhang <1743067175@qq.com>
2023-05-05 11:34:19 +0800

    feat: docusaurus init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('D:\\code\\react-docs-cn\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\code\\react-docs-cn\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\code\\react-docs-cn\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('D:\\code\\react-docs-cn\\src\\css\\custom.css'),
+];
